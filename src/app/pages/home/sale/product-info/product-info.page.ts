b91ac2e71a453daf9bd43cc373cccbe7279a6179
alireza <alireza.jvh98@gmail.com>
2020-04-10 16:05:14 +0430

    create product-info page

---
+++ src/app/pages/home/sale/product-info/product-info.page.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'sale-product-info',
+    templateUrl: './product-info.page.html',
+    styleUrls: ['./product-info.page.scss']
+})
+
+export class ProductInfoPage {
+    
+}
